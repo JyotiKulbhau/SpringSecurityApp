9dc9d094d0641a33c7af30d77028396c28a36235
Jyoti Kulbhau <jyoti.kulbhau@viraltech.in>
2025-05-05 17:45:36 +0530

    show userName after login

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +16,20 @@ public class HomeController {
 
 	@PreAuthorize("hasRole('USER')")
 	@GetMapping("/user")
-	public ResponseEntity<String> normalUser() {
-		return ResponseEntity.ok("Normal User");
+	public ResponseEntity<String> normalUser(Authentication authentication) {
+		String username = authentication.getName();
+		String role = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+				.collect(Collectors.joining(", "));
+		return ResponseEntity.ok("Hi " + username + ", your role is :" + role);
 	}
 
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/admin")
-	public ResponseEntity<String> adminUser() {
-		return ResponseEntity.ok("Admin User");
+	public ResponseEntity<String> adminUser(Authentication authentication) {
+		String uName = authentication.getName();
+		String role = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+				.collect(Collectors.joining(", "));
+		return ResponseEntity.ok("Hi " + uName + ", your role is :" + role);
 	}
 
 	@GetMapping("/public")
